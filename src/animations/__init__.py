560a117729e73eb46e01ef50e6485fb2a6abbad8
oceanskibum <schiski@gmail.com>
2025-04-24 18:53:59 -0600

    Add files via upload

---
+++ src/animations/__init__.py
@@ -0,0 +1,13 @@
+# 🎛 Master Animation Mode Index
+
+# List of officially supported visual modes
+ANIMATION_MODES = [
+    "house_mode",
+    "bass_mode",
+    "techno_mode",
+    "basshouse_mode",
+    "fade_transition",
+    "idle_mode",
+    "dancer_mode",
+    "hypno_mode"
+]
